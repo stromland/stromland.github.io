c972bccb430ea8fd86153f8f825e1df7aba9b451
Espen Strømland <Espen.Stromland@skatteetaten.no>
2021-06-27 22:31:36 +0200

    layout start

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,8 +3,15 @@ import { FC } from 'react';
 export const Layout: FC = ({ children }) => {
   return (
     <div className="container-fluid">
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <span className="navbar-brand mb-0 h1">stromland.github.io</span>
+        </div>
+      </nav>
       <div className="row">
-        <div className="col">{children}</div>
+        <div className="col"></div>
+        <div className="col-6 px-5 py-3 bg-light">{children}</div>
+        <div className="col"></div>
       </div>
     </div>
   );
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { NotFound } from './components/NotFound';
+import { Layout } from './components/Layout';
 import { MarkdownViewer } from './containers/MarkdownViewer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
@@ -9,13 +10,15 @@ import './index.css';
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Switch>
-        <Route exact={true} path="/" component={MarkdownViewer} />
-        <Route path="/about" component={MarkdownViewer} />
-        <Route path="*">
-          <NotFound />
-        </Route>
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route exact={true} path="/" component={MarkdownViewer} />
+          <Route path="/about" component={MarkdownViewer} />
+          <Route path="*">
+            <NotFound />
+          </Route>
+        </Switch>
+      </Layout>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
